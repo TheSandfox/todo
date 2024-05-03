d0a37bab6c4fa05ce3364c62231626b9634851d6
thesandfox <sandfox9411@gmail.com>
2024-05-03 13:56:31 +0900

    240503333

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Todo from 'components/Todo';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 function App() {
-  return <Routes>
-    	<Route exact path={'/'} element={<Todo checkOnly={false}/>}/>
-    	<Route path={'/checked/*'} element={<Todo checkOnly={true}/>}/>
-  </Routes>
+	// return <Todo checkOnly={false}/>
+	return <BrowserRouter basename={process.env.PUBLIC_URL}>
+	<Routes>
+			<Route exact path={'/'} element={<Todo checkOnly={false}/>}/>
+			<Route path={'/checked/*'} element={<Todo checkOnly={true}/>}/>
+	</Routes>
+  </BrowserRouter>
 }
 export default App;
